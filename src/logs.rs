f0e83fbb4962aafdf7599db7c2c2a077f9f7023f
Tomáš Novák <tomas.novak@olc.cz>
2026-04-20 19:52:17 +0200

    fix: change the order of outputing the initial decal and infoline

--- src/logs.rs
+++ src/logs.rs
@@ -7,23 +7,6 @@ use tracing_subscriber::{
 };
 
 pub fn setup_tracing() {
-    tracing::info!(
-        "Starting image proxy server version {}",
-        env!("CARGO_PKG_VERSION")
-    );
-    tracing::info!(
-        r"
- _                                                             
-(_)                                                            
- _ _ __ ___   __ _  __ _  ___        _ __  _ __ _____  ___   _ 
-| | '_ ` _ \ / _` |/ _` |/ _ \______| '_ \| '__/ _ \ \/ / | | |
-| | | | | | | (_| | (_| |  __/______| |_) | | | (_) >  <| |_| |
-|_|_| |_| |_|\__,_|\__, |\___|      | .__/|_|  \___/_/\_\\__, |
-                    __/ |           | |                   __/ |
-                   |___/            |_|                  |___/ 
-    "
-    );
-
     tracing_subscriber::registry()
         .with(fmt::layer().with_span_events(FmtSpan::CLOSE))
         .with(
@@ -32,4 +15,21 @@ pub fn setup_tracing() {
                 .from_env_lossy(),
         )
         .init();
+
+    tracing::info!(
+        "Starting image proxy server version {}",
+        env!("CARGO_PKG_VERSION")
+    );
+    tracing::info!(
+        r"
+     _                                                             
+    (_)                                                            
+     _ _ __ ___   __ _  __ _  ___        _ __  _ __ _____  ___   _ 
+    | | '_ ` _ \ / _` |/ _` |/ _ \______| '_ \| '__/ _ \ \/ / | | |
+    | | | | | | | (_| | (_| |  __/______| |_) | | | (_) >  <| |_| |
+    |_|_| |_| |_|\__,_|\__, |\___|      | .__/|_|  \___/_/\_\\__, |
+                        __/ |           | |                   __/ |
+                       |___/            |_|                  |___/ 
+    "
+    );
 }
